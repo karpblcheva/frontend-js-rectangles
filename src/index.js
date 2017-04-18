910b5d83083ee07a79cdee903f1dc26a23ed5652
karpblcheva <m.karpblcheva@gmail.com>
2017-04-18 04:38:04 +0300

    develop function areIntersected

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-﻿
+﻿var r1 = {
+	left: 0,
+	top: 0,
+	width: 30,
+	height: 30,
+ };
+
+ var r2 = {
+	 left: 30,
+	 top: 10,
+	 width: 30,
+	 height: 10
+ };
+
+function right (rectangle) {
+	return rectangle.left + rectangle.width;
+}
+function bottom (rectangle) {
+	return rectangle.top + rectangle.height;
+}
+
+r1.right = right(r1);
+r1.bottom = bottom(r1);
+r2.right = right(r2);
+r2.bottom = bottom(r2);
+
+function areIntersected( r1, r2 ) {
+	if ( r1.bottom <= r2.top || r1.right <= r2.left || r2.bottom <= r1.top || r2.right <= r1.left )
+    return false;
+	else return true;
+}
+
+console.log(areIntersected( r1, r2 ))
+ 
+  
